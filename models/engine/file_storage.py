d6f8a620805970026fc940ed57a58e3d9c3dc877
Constance Amatey <consamate57@gmail.com>
2023-10-13 00:17:54 +0000

    serialization and deserialization of strings

---
+++ models/engine/file_storage.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" creates a class called filestorage"""
+
+import models
+import json
+
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+
+def all(self):
+    """
+    public method that returns dictionary objects
+    """
+    return self.__objects
+
+
+def new(self, obj):
+    """
+    public methods that sets value in dictionary object
+
+    Args:
+        obj (_type_): value to be set
+    """
+    key = obj.__class__.__name__ + "." + obj.id
+    self.__objects[key] = obj
+
+
+def save(self):
+    """ method that serializes a dictionary to json file"""
+    objDict = {}
+    for key, val in FileStorage.__objects.items():
+        objDict[key] = val.toDict()
+
+    with open(FileStorage.__file_path, "w", encoding="utf-8") as fd:
+        json.dump(objDict, fd)
+
+
+def reload(self):
+    """deserialises json string to dict string"""
+    objDict = None
+    try:
+        for key, val in FileStorage.__objects.items():
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as fd:
+                objDict = json.load(fd)
+    except FileNotFoundError:
+        pass
