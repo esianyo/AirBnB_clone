0a7449bef916e262f37d277a4099ecb4ba566c1c
Constance Amatey <consamate57@gmail.com>
2023-10-13 00:43:34 +0000

    console

---
+++ console.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""program that contains the entry point of the command interpreter"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """ class method for command line entry point"""
+    
