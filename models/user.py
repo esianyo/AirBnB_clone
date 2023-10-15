990d70e8c949e5c43b3a4d46d38878815d650aa7
Constance Amatey <consamate57@gmail.com>
2023-10-15 21:40:21 +0000

    user class

---
+++ models/user.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import uuid
+from datetime import datetime
+from models import storage
+from models.base_model import BaseModel
+
+
+"""user module"""
+
+
+class User(BaseModel):
+    """class for the airbnb user"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """method to handle user commands"""
+        if len(kwargs) > 0:
+            self.__dict__ = kwargs
+        else:
+            super().__init__(self)
