3822a1765efd98de2920bc4e5bafec477dea2504
Constance Amatey <consamate57@gmail.com>
2023-10-16 21:26:26 +0000

    worked on attributes

--- models/base_model.py
+++ models/base_model.py
@@ -13,15 +13,25 @@ class BaseModel:
     """
 
     def __init__(self, *args, **kwargs):
-        """initializing public attributes for id, date, and time"""
-        if len(kwargs) != 0:
-            self.__dict__ = kwargs
-            self.created_at = datetime.strptime(kwargs.get("created_at"),
-                                                "%Y-%m-%dT%H:%M:%S.%f")
-        else:
-            self.id = str(uuid.uuid1())
+
+        import models
+        '''
+            Initialize public instance attributes.
+        '''
+        if (len(kwargs) == 0):
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            storage.new(self)
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+        else:
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            for key, val in kwargs.items():
+                if "__class__" not in key:
+                    setattr(self, key, val)
+
 
     def save(self):
         """ updates date and time after changes have been made"""
