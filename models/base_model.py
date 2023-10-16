09fba127e70403d7ac5ab8905cfe3ce2b6c18c97
Constance Amatey <consamate57@gmail.com>
2023-10-16 23:01:47 +0000

    resolved more issues

--- models/base_model.py
+++ models/base_model.py
@@ -14,27 +14,15 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
 
-        import models
-        '''
-            Initialize public instance attributes.
-        '''
-        if (len(kwargs) == 0):
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
+        if len(kwargs) is not 0:
+            self.__dict__ = kwargs
+            self.created_at = datetime.strptime(kwargs.get("created_at"),
+                                                "%Y-%m-%dT%H:%M:%S.%f")
         else:
-            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
-                                                     "%Y-%m-%dT%H:%M:%S.%f")
-            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
-                                                     "%Y-%m-%dT%H:%M:%S.%f")
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    dtm_obj = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, dtm_obj)
-                else:
-                    setattr(self, key, value)
-
+            self.id = str(uuid.uuid1())
+            self.created_at = datetime.now()
+            storage.new(self)
+        
 
     def save(self):
         """ updates date and time after changes have been made"""
@@ -47,7 +35,8 @@ class BaseModel:
         Returns:
             str: paired values in dictionary format
         """
-        return "\n".join([f"{key}: {value}" for key, value in self.__dict__.items()])
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                                      self.id, self.__dict__))
 
     def to_json(self):
         """returns values in json format"""
