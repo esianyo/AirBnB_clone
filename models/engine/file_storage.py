9ce981219141b789ab82791f6a7deb22afa8a551
esianyo <esianicd@gmail.com>
2023-10-16 02:17:33 +0000

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,63 +1,47 @@
 #!/usr/bin/python3
-""" creates a class called filestorage"""
+"""Type module FileStorage"""
 
-import models
+import os.path
 import json
 from models.base_model import BaseModel
-from models.user import User
-from models.city import City
-from models.place import Place
-from models.state import State
 from models.amenity import Amenity
+from models.place import Place
+from models.user import User
+from models.state import State
 from models.review import Review
-
-classes = {'BaseModel': BaseModel, 'User': User, 'Place': Place,
-           'State': State, 'City': City, 'Amenity': Amenity,
-           'Review': Review}
+from models.city import City
 
 
 class FileStorage:
+    """Type class File Storage"""
     __file_path = "file.json"
     __objects = {}
 
+    def all(self):
+        """Type method all"""
+        return FileStorage.__objects
 
-def all(self):
-    """
-    public method that returns dictionary objects
-    """
-    return self.__objects
+    def new(self, obj):
+        """Type method new"""
+        FSobjdict = FileStorage.__objects
+        object_name = obj.__class__.__name__
+        FSobjdict["{}.{}".format(object_name, obj.id)] = obj
 
+    def save(self):
+        """Type method save"""
+        FSobjdict = FileStorage.__objects
+        obj_dict = {obj: FSobjdict[obj].to_dict() for obj in FSobjdict.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(obj_dict, f)
 
-def new(self, obj):
-    """
-    public methods that sets value in dictionary object
+    def reload(self):
+        """Type method reaload"""
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path) as f:
+                obj_dict = json.load(f)
+                for obj in obj_dict.values():
+                    cls_d = obj["__class__"]
+                    del obj["__class__"]
+                    self.new(eval(cls_d)(**obj))
+        return
 
-    Args:
-        obj (_type_): value to be set
-    """
-    key = obj.__class__.__name__ + "." + obj.id
-    self.__objects[key] = obj
-
-
-def save(self):
-    """ method that serializes a dictionary to json file"""
-    objDict = {}
-    for key, val in FileStorage.__objects.items():
-        objDict[key] = val.toDict()
-
-    with open(FileStorage.__file_path, "w", encoding="utf-8") as fd:
-        json.dump(objDict, fd)
-
-
-def reload(self):
-    """deserialises json string to dict string"""
-    objDict = None
-    try:
-        with open(FileStorage.__file_path, "r", encoding="utf-8") as fd:
-            objDict = json.load(fd)
-        for key, val in FileStorage.__objects.items():
-            class_name = val["__class__"]
-            class_name = models.classes[class_name]
-            FileStorage.__objects[key] = class_name(**val)
-    except FileNotFoundError:
-        pass
